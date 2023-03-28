c8a1c19ae4a043f0ba971a8fbbe44138d145a6c2
ijon <ijontan9@gmail.com>
2023-03-29 02:17:30 +0800

    fixed the split_arg function to do stuff properly

--- src/utils/split_args.c
+++ src/utils/split_args.c
@@ -6,7 +6,7 @@
 /*   By: itan <itan@student.42kl.edu.my>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/29 00:36:04 by itan              #+#    #+#             */
-/*   Updated: 2023/03/29 01:28:50 by itan             ###   ########.fr       */
+/*   Updated: 2023/03/29 02:06:29 by itan             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,21 +17,24 @@ static char	**split_recurse(char *command, int depth)
 	char	*str;
 	char	**dst;
 	int		i;
-	bool	is_quoted;
+	int		is_quoted;
 
-	i = -1;
-	is_quoted = false;
-	while ((command[++i] != ' ' || is_quoted) && command[i])
-		if (command[i] == '\"')
-			is_quoted = !is_quoted;
-	str = ft_substr(command, 0, i);
+	is_quoted = 0;
+	while (*command == ' ')
+		command++;
+	if (*command == '"' || *command == '\'')
+		is_quoted = *command++;
+	i = 0;
+	while ((command[i] != ' ' || is_quoted) && command[i])
+		if (command[i++] == is_quoted)
+			break ;
+	str = ft_substr(command, 0, i - !(!is_quoted));
+	while (command[i] && command[i] == ' ')
+		i++;
 	if (!command[i])
-	{
 		dst = (char **)ft_calloc(depth + 2, sizeof(char *));
-		dst[depth] = str;
-		return (dst);
-	}
-	dst = split_recurse(command + i + 1, depth + 1);
+	else
+		dst = split_recurse(command + i, depth + 1);
 	dst[depth] = str;
 	return (dst);
 }
