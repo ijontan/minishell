d6c2a6f64b033ff4b4082741c921167713c1d1be
ijon <itan@student.42kl.edu.my>
2023-03-29 21:06:41 +0800

    update signal

--- src/signals/handle_signal.c
+++ src/signals/handle_signal.c
@@ -6,7 +6,7 @@
 /*   By: itan <itan@student.42kl.edu.my>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/28 17:02:38 by itan              #+#    #+#             */
-/*   Updated: 2023/03/28 19:02:34 by itan             ###   ########.fr       */
+/*   Updated: 2023/03/29 19:19:36 by itan             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,8 +15,5 @@
 void	handle_signal(int signo)
 {
 	if (signo == SIGINT)
-	{
-		ft_printf("\b\b   \n");
-		ft_printf("minishell$ ");
-	}
+		ft_printf("\b\b  \n%s", rl_prompt);
 }
