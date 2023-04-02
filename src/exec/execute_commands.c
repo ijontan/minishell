7fac00a8328224979a14122915233b38a9c8a53f
ijon <ijontan9@gmail.com>
2023-04-02 22:33:58 +0800

    fixed exec command

--- src/exec/execute_commands.c
+++ src/exec/execute_commands.c
@@ -6,7 +6,7 @@
 /*   By: itan <itan@student.42kl.edu.my>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/02 21:59:23 by itan              #+#    #+#             */
-/*   Updated: 2023/04/02 22:21:57 by itan             ###   ########.fr       */
+/*   Updated: 2023/04/02 22:33:16 by itan             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,16 +28,16 @@ static int	exec_command(t_sh_data *sh_data, int i)
 	pid = fork();
 	if (pid == 0)
 	{
-		if (cmd->in_file)
-			dup2(cmd->in_fd, 0);
-		else if (i > 0 && !sh_data->commands[i - 1].out_file)
-			dup2(sh_data->pipes[i - 1]->pipe[1], 0);
-		if (cmd->out_file)
-			dup2(cmd->out_fd, 1);
-		else if (sh_data->commands[i + 1])
-			dup2(sh_data->pipes[i]->pipe[0], 1);
-		execve(cmd->path, cmd->args, sh_data->env);
-		printf("minishell: %s: %s\n", cmd->args[0], strerror(errno));
+		if (cmd->fd_in)
+			dup2(cmd->fd_in, 0);
+		else if (i > 0 && !sh_data->commands[i - 1].fd_out)
+			dup2(sh_data->pipes[i - 1].pipe[1], 0);
+		if (cmd->fd_out)
+			dup2(cmd->fd_out, 1);
+		else if (sh_data->commands[i + 1].fd_in)
+			dup2(sh_data->pipes[i].pipe[0], 1);
+		execve(cmd->program, cmd->args, sh_data->env);
+		perror("minishell");
 		exit(1);
 	}
 	return (pid);
@@ -54,14 +54,13 @@ void	exec_commands(t_sh_data *sh_data)
 	int	*pids;
 
 	i = 0;
-	while (sh_data->commands[i])
+	while (sh_data->commands[i].program)
 		i++;
 	pids = (int *)ft_calloc(i + 1, sizeof(int));
 	i = -1;
-	while (sh_data->commands[++i])
-		if (sh_data->commands[i])
-			pids[i] = exec_command(sh_data, i);
+	while (sh_data->commands[++i].program)
+		pids[i] = exec_command(sh_data, i);
 	i = -1;
-	while (sh_data->commands[++i])
+	while (sh_data->commands[++i].program)
 		waitpid(pids[i], NULL, 0);
 }
