f3f7bdc5f082c1096310f67158bb8c02cb967466
ijon <ijontan9@gmail.com>
2023-04-02 22:46:12 +0800

    even more work on exec

--- src/exec/execute_commands.c
+++ src/exec/execute_commands.c
@@ -6,11 +6,36 @@
 /*   By: itan <itan@student.42kl.edu.my>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/02 21:59:23 by itan              #+#    #+#             */
-/*   Updated: 2023/04/02 22:33:16 by itan             ###   ########.fr       */
+/*   Updated: 2023/04/02 22:45:37 by itan             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+
+//! everything in this file is not tested yet
+
+static void	setup_pipes(t_sh_data *sh_data, int n)
+{
+	int	i;
+
+	sh_data->pipes = (t_pipe *)ft_calloc(n, sizeof(t_pipe));
+	i = -1;
+	while (i < n - 1)
+		pipe(sh_data->pipes[i].pipe);
+}
+
+static void	close_pipes(t_sh_data *sh_data)
+{
+	int	i;
+
+	i = -1;
+	while (sh_data->pipes[++i].pipe[0])
+	{
+		close(sh_data->pipes[i].pipe[0]);
+		close(sh_data->pipes[i].pipe[1]);
+	}
+	free(sh_data->pipes);
+}
 
 /**
  * @brief execute a single command
@@ -36,6 +61,7 @@ static int	exec_command(t_sh_data *sh_data, int i)
 			dup2(cmd->fd_out, 1);
 		else if (sh_data->commands[i + 1].fd_in)
 			dup2(sh_data->pipes[i].pipe[0], 1);
+		close_pipes(sh_data);
 		execve(cmd->program, cmd->args, sh_data->env);
 		perror("minishell");
 		exit(1);
@@ -56,10 +82,12 @@ void	exec_commands(t_sh_data *sh_data)
 	i = 0;
 	while (sh_data->commands[i].program)
 		i++;
+	setup_pipes(sh_data, i);
 	pids = (int *)ft_calloc(i + 1, sizeof(int));
 	i = -1;
 	while (sh_data->commands[++i].program)
 		pids[i] = exec_command(sh_data, i);
+	close_pipes(sh_data);
 	i = -1;
 	while (sh_data->commands[++i].program)
 		waitpid(pids[i], NULL, 0);
