0ce46e8913bc1e0e0a5fe998df69fef13ec5b79c
Ng Wai Keat <nwai-kea@u90z04s06.42kl.edu.my>
2023-06-20 23:43:18 +0800

    restructured cd

--- src/build_in/cd.c
+++ src/build_in/cd.c
@@ -6,7 +6,7 @@
 /*   By: nwai-kea <nwai-kea@student.42kl.edu.my>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/29 16:46:06 by itan              #+#    #+#             */
-/*   Updated: 2023/05/30 23:27:32 by nwai-kea         ###   ########.fr       */
+/*   Updated: 2023/06/20 23:42:43 by nwai-kea         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,7 +29,7 @@ static void	*ft_memdel(void *ptr)
 	return (NULL);
 }
 
-static int	update_oldpwd(void)
+static int	update_oldpwd(char **env)
 {
 	char	*oldpwd;
 	char	*cwd;
@@ -38,9 +38,13 @@ static int	update_oldpwd(void)
 	if (!cwd)
 		return (0);
 	oldpwd = ft_strjoin("OLDPWD=", cwd);
+	if (!check_program_exist(oldpwd, env))
+		add_env(oldpwd, env);
+	ft_memdel(oldpwd);
+	return (1);
 }
 
-static int	to_path(int path)
+static int	to_path(int path, t_sh_data *data)
 {
 	int		ret;
 	char	*env_path;
@@ -48,7 +52,8 @@ static int	to_path(int path)
 	env_path = NULL;
 	if (path == 0)
 	{
-		env_path = get_path("HOME", 4);
+		update_oldpwd(data->env);
+		env_path = check_program_exist("HOME", data->env);
 		if (env_path == NULL)
 			ft_putendl_fd("minishell : cd : HOME not set", STDERR);
 		if (env_path == NULL)
@@ -56,11 +61,12 @@ static int	to_path(int path)
 	}
 	else if (path == 1)
 	{
-		env_path = get_path("OLDPWD", 6);
+		env_path = check_program_exist("OLDPWD", data->env);
 		if (env_path == NULL)
 			ft_putendl_fd("minishell : cd : OLDPWD not set", STDERR);
 		if (env_path == NULL)
 			return (1);
+		update_oldpwd(data->env);
 	}
 	ret = chdir(env_path);
 	ft_memdel(env_path);
@@ -72,30 +78,31 @@ static int	check_args(char **args)
 	char	*cwd;
 	char	buffer[4097];
 
-	if (args[1])
+	if (args[0])
 	{
-		if (args[2])
+		if (args[1])
 		{
 			ft_putendl_fd("minishell : cd : too many arguments\n", STDERR);
 			return (1);
 		}
 		cwd = getcwd(buffer, 4096);
 	}
+	return (0);
 }
 
-void	cd(char **args)
+void	cd(char **args, t_sh_data *data)
 {
-	if (args[1] == NULL || args[1], "~")
+	if (args[0] == NULL || args[0] == "~")
 		return (to_path(0));
-	if (chdir(args[1]) != 0)
+	if (chdir(args[0]) != 0)
 	{
 		perror("minishell");
 	}
 	else
 	{
-		if (args[1][0] == '-' && !args[1][2])
+		if (args[0][0] == '-' && !args[0][2])
 		{
-			return (to_path(1));
+			return (to_path(1, data));
 		}
 	}
 }
