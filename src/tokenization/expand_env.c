247aef258f94dd11e6540ae198bd3f1e74c146d5
ijon <itan@student.42kl.edu.my>
2023-06-26 14:54:05 +0800

    deprecation

--- src/tokenization/expand_env.c
+++ src/tokenization/expand_env.c
@@ -6,19 +6,18 @@
 /*   By: itan <itan@student.42kl.edu.my>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/02 21:47:51 by itan              #+#    #+#             */
-/*   Updated: 2023/04/03 14:20:58 by itan             ###   ########.fr       */
+/*   Updated: 2023/06/26 14:53:30 by itan             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
-//TODO: implement this function
 
 /**
- * @brief expand_env should expand the environment variables in the string
- * 
- * @param str 
+ * @brief  (depricated)
+ *
+ * @param str
  * @param env the current environment variables
- * @return char* 
+ * @return char*
  */
 char	*expand_env(char *str, char **env)
 {
