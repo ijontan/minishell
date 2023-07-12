7876d149a1c1a55e6d09de35411f107754d23e17
Ng Wai Keat <nwai-kea@u90z04s06.42kl.edu.my>
2023-07-12 18:38:32 +0800

    more parentheses stuff

--- src/exec/parentheses.c
+++ src/exec/parentheses.c
@@ -6,27 +6,35 @@
 /*   By: nwai-kea <nwai-kea@student.42kl.edu.my>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/10 22:13:15 by nwai-kea          #+#    #+#             */
-/*   Updated: 2023/07/11 00:41:48 by nwai-kea         ###   ########.fr       */
+/*   Updated: 2023/07/12 18:03:05 by nwai-kea         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
+t_command_chunk	*init_brackets(t_command_chunk *chunk)
+{
+	chunk = (t_command_chunk *)malloc(sizeof(t_command_chunk));
+	chunk->count = 0;
+	chunk->subshell = false;
+	return (chunk);
+}
+
 int	detect_brackets(t_command_chunk *chunk, char *cmd)
 {
-	int	i;
-
-	i = 0;
-	chunk->count = 0;
-	while (cmd && cmd[i])
+	while (*cmd)
 	{
-		if (cmd[i] == '(')
+		if (*cmd == '(')
+		{
+			if (chunk->count == 0)
+				chunk->commands->content = cmd;
 			chunk->count++;
-		if (cmd[i] == ')')
+		}
+		else if (*cmd == ')')
 			chunk->count--;
 		if (chunk->count > 0)
 			chunk->subshell = true;
-		i++;
+		cmd++;
 	}
 	if (chunk->count != 0 && !cmd)
 		return (1);
