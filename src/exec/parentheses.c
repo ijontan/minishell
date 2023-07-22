74b595aad322acfe8f46f366f953a64946381fd1
Tapey-ui <jackng101121@gmail.com>
2023-07-23 03:15:10 +0800

    parentheses update

--- src/exec/parentheses.c
+++ src/exec/parentheses.c
@@ -6,46 +6,95 @@
 /*   By: nwai-kea <nwai-kea@student.42kl.edu.my>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/10 22:13:15 by nwai-kea          #+#    #+#             */
-/*   Updated: 2023/07/22 14:16:54 by nwai-kea         ###   ########.fr       */
+/*   Updated: 2023/07/23 01:22:55 by nwai-kea         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-t_command_chunk	*init_brackets(t_command_chunk *chunk)
+int	get_cmd_len(char *line, int i)
 {
-	chunk = (t_command_chunk *)malloc(sizeof(t_command_chunk));
-	chunk->count = 0;
-	chunk->subshell = false;
-	return (chunk);
-}
+	int	count;
+	int	len;
 
-int	detect_brackets(t_command_chunk *chunk, char *cmd)
-{
-	while (*cmd)
+	count = 0;
+	len = 0;
+	while (line[i])
 	{
-		if (*cmd == '(')
-		{
-			if (chunk->count == 0)
-				chunk->commands->content = cmd;
-			chunk->count++;
-		}
-		else if (*cmd == ')')
-			chunk->count--;
-		if (chunk->count > 0)
-			chunk->subshell = true;
-		cmd++;
+		if (line[i] == '(')
+			count++;
+		if (line[i] == ')')
+			count--;
+		if (!count)
+			break ;
+		len++;
+		i++;
 	}
-	if (chunk->count != 0 && !cmd)
-		return (1);
-	return (0);
+	return (len);
 }
 
-// char	*parentheses(t_command_chunk *chunk, char *cmd)
-// {
-// 	if (detect_brackets(chunk, cmd))
-// 	{
-// 		perror("incomplete parentheses");
-// 		return (NULL);
-// 	}
-// }
+char	*subshell(char *line, int i)
+{
+	char	*sub_cmd;
+	int		len;
+
+	len = get_cmd_len(line, i);
+	sub_cmd = (char *)malloc(len * sizeof(char));
+	ft_strlcpy(sub_cmd, &line[i + 1], len);
+	return (sub_cmd);
+}
+
+int	detect_brackets(char *cmd)
+{
+	int		count;
+	int		i;
+	char	*line;
+	bool	subshell;
+
+	i = 0;
+	count = 0;
+	line = NULL;
+	subshell = false;
+	while (cmd[i])
+	{
+		if (cmd[i] == '(')
+			count++;
+		if (cmd[i] == ')')
+			count--;
+		i++;
+	}
+	if (count < 0 || count > 0)
+		return (1);
+	else
+		return (0);
+}
+
+char	*parentheses(char *line)
+{
+	int		i;
+	char	*cmd;
+	pid_t	pid;
+
+	// char	**comm;
+	cmd = NULL;
+	if (detect_brackets(line))
+	{
+		perror("incomplete parentheses");
+		return (NULL);
+	}
+	else
+	{
+		i = 0;
+		while (line[i])
+		{
+			if (line[i] == '(')
+				cmd = subshell(line, i);
+			i++;
+		}
+		pid = fork();
+		if (pid == 0)
+		{
+		}
+	}
+	return (NULL);
+}
